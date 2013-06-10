be88acec168362702a950b80b2a7bcfed4a27def
OpenShift guest <deannah@mit.edu>
2013-06-10 10:41:58 -0400

    changes this morning

---
+++ day1.js
@@ -0,0 +1,6 @@
+ function calculate() {
+                var input = $('#text1:first'); // get input field
+                var val = input.val();
+                var output = $('#text1_out:first');
+                output.text(val);
+            }
