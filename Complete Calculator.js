5ad8996232bad122f1a1d18d95bcf98088910167
OpenShift guest <deannah@mit.edu>
2013-06-17 02:16:42 -0400

    Began adding graphing calculator functionality

--- Complete Calculator.js
+++ Complete Calculator.js
@@ -81,6 +81,68 @@ function evaluate(array) {
     //     }
     // };
 
+    var graph = function(canvas, func, min, max) {
+        var DOMcanvas = canvas[0];
+        var width=canvas.width();
+        var height=canvas.height();
+        DOMcanvas.width = width;
+        DOMcanvas.height = height;
+        var ctx = DOMcanvas.getContext('2d');
+        
+        try{
+        var tree= calculator.parse(func);
+        var output=[];
+        for(var i=0;i<width;i++){
+            var value=calculator.evaluate(tree,{x:min+1.0*i/(width-1)*(max-min),e:Math.e,pi:Math.PI});//1.0* to make it floating
+            output.push(value);
+        }
+        var maxx=Math.max.apply(Math, output);//max amp
+        var minn=Math.min.apply(Math,output);//min amp
+            
+        var gcanvas = $("<canvas></canvas>")[0]; //DOM object
+        gcanvas.width = width;
+        gcanvas.height = height-10;
+        var gheight=height-10;
+        var gctx = gcanvas.getContext('2d');
+        gctx.beginPath();
+        gctx.moveTo(0,gheight-(output[0]-minn)/(maxx-minn)*gheight);
+        for(var j=1;j<width;j++){
+            gctx.lineTo(j,gheight-(output[j]-minn)/(maxx-minn)*gheight);
+        }
+        gctx.stroke();
+        
+        ctx.drawImage(gcanvas,0,5);// to leave upper and lower border by 5 pixcels
+        canvas.on("mousemove",function(event){
+            var mx = event.pageX;
+            var my = event.pageY;
+            var offset = canvas.offset();//{left: ..., top: ...}
+            mx = Math.round(mx-offset.left);
+            my=Math.round(my-offset.top);
+            
+            ctx.clearRect(0,0,width,height);
+            ctx.drawImage(gcanvas,0,5);// to leave upper and lower border by 5 pixcels
+            
+            ctx.beginPath(); //drawing a crosshair over cursor location
+            ctx.moveTo(mx-10, my);
+            ctx.lineTo(mx+10, my);
+            ctx.moveTo(mx, my-10);
+            ctx.lineTo(mx, my+10);
+            ctx.strokeStyle="black";
+            ctx.lineWidth = 1;
+            ctx.stroke();
+            
+        });
+        } catch(err){
+            // error on canvas
+            ctx.beginPath();
+            ctx.fillStyle = "black";
+            ctx.font = "14px Arial";
+            ctx.textAlign = "center"; //left, right
+            ctx.textBaseline = "middle"; //top, bottom, alphabetic
+            ctx.fillText(err, width/2, height/2);//string, x, y
+        }
+    };
+
     function setup_grapher() {
         var div = $(".calculator");
         var graphDiv = $("<div class='grapher background'></div>");
@@ -105,6 +167,10 @@ function evaluate(array) {
         
         graphDiv.append(canvasDiv, functionDiv, xDiv);
         div.append(graphDiv);
+        
+        $('.plotButton').on("click", function () {
+            graph(canvas1, input1.val(), input2.val(), input3.val());
+        });
     }
 
     function setup_calculator(div) { //for the simple, apple calculator
