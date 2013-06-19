59ccd90ff80a0eb0721b44faa7bedc3991ecb229
OpenShift guest <deannah@mit.edu>
2013-06-19 09:35:22 -0400

    Adding Day 8 Files

--- Day8Visuals/visuals.js
+++ Day8Visuals/visuals.js
@@ -2,11 +2,16 @@
 
 var data= [0, 4, 8, 8, 15, 16, 23, 42];
 
-var chart = $("<div></div>").addClass("chart");
+// var chart = $("<div></div>").addClass("chart");
 
-$(".chart-container").append(chart);
+// $(".chart-container").append(chart);
 
 //data.forEach(function(d) {chart.append(d);});
-data.forEach(function(d) {
-    chart.append($("<div></div>").css("width", d * 10 + "px").text(d));
-    });
+// data.forEach(function(d) {
+//     chart.append($("<div></div>").css("width", d * 10 + "px").text(d));
+// });
+    
+    
+var chart = d3.select(".chart-container")
+        .append("div")
+        .attr("class", "chart");
